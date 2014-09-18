85cb41bb54f5e32cd08c29067ac77ca5c391322d
Genc Mazlami <genc.mazlami@uzh.ch>
2014-09-18 21:44:50 +0200

    Initial import

---
+++ src/com/example/uzhvorlesungen/parsers/VVZLecturesParser.java
@@ -0,0 +1,48 @@
+package com.example.uzhvorlesungen.parsers;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.htmlparser.Node;
+import org.htmlparser.Parser;
+import org.htmlparser.Tag;
+import org.htmlparser.filters.HasAttributeFilter;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+
+public class VVZLecturesParser {
+
+	private String resourceURL = null;
+	private String URLprefix = null;
+	private HashMap<String, String> linkMap = new HashMap<String, String>();
+
+	public VVZLecturesParser(String url, String prefix) {
+		this.resourceURL = url;
+		this.URLprefix = prefix;
+	}
+
+	public Map<String, String> parse() throws ParserException {
+		Parser parser = new Parser();
+		parser.setResource(resourceURL);
+		HasAttributeFilter f = new HasAttributeFilter("class", "internal");
+		NodeList nl = parser.parse(f);
+
+		String str = null;
+		String link = null;
+		Node node = null;
+
+		for (int i = 0; i < nl.size(); i++) {
+			node = nl.elementAt(i);
+			str = nl.elementAt(i).toPlainTextString().trim();
+
+			if (node instanceof Tag) {
+				link = ((Tag) node).getAttribute("href");
+				link = link.substring(6);
+				linkMap.put(Utils.fixUmlauts(str), URLprefix + link);
+			}
+		}
+
+		return linkMap;
+	}
+}
---
+++ src/com/example/uzhvorlesungen/parsers/VVZStudiesParser.java
@@ -0,0 +1,99 @@
+package com.example.uzhvorlesungen.parsers;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.htmlparser.Node;
+import org.htmlparser.Parser;
+import org.htmlparser.Tag;
+import org.htmlparser.filters.HasAttributeFilter;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+
+public class VVZStudiesParser {
+
+	String URLPrefix = null;
+	String URL = null;
+	
+	HashMap<String, String> studiesMap = new HashMap<String, String>();
+	HashMap<String, List<String>> facultyMap = new HashMap<String, List<String>>();
+
+	public VVZStudiesParser(String url, String prefix) throws ParserException {
+		URL = url;
+		URLPrefix = prefix;
+	}
+
+	public Map<String, List<String>> parseFaculties() throws ParserException {
+		HasAttributeFilter f = new HasAttributeFilter("class", "links");
+		Parser parser = new Parser();
+		parser.setResource(URL);
+		NodeList nl = parser.parse(f);
+		String faculty = null;
+		String facName = null;
+
+		for (int i = 0; i < nl.size(); i++) {
+
+			Node node = nl.elementAt(i);
+			faculty = node.getFirstChild().getFirstChild().getFirstChild().getText().trim();
+			if(faculty.contains("&auml;")){
+				facName = faculty.replace("&auml;", "ä");
+			}else{
+				facName = faculty;
+			}
+			
+			
+			NodeList childList = node.getChildren();
+			NodeList listChilds = childList.elementAt(1).getChildren();
+			
+			List<String> list = new ArrayList<String>();
+			for(int j = 0 ; j < listChilds.size(); j++){
+				String str = listChilds.elementAt(j).toPlainTextString().trim();
+				String name = null;
+				
+				if (!str.contains("&uuml;") && !str.contains("&ouml;")
+						&& !str.contains("&Auml") && !str.contains("Weiter")
+						&& !str.contains("Double") && !str.contains("Joint")) {
+					
+					if(str.contains("&auml;")){
+						name = str.replace("&auml;", "ä");
+						list.add(name);
+					}else{
+						list.add(str);
+					}
+					
+				}
+			}
+			facultyMap.put(facName, list);
+		}
+		return facultyMap;
+	}
+	
+	public Map<String, String> parseStudies() throws ParserException {
+		HasAttributeFilter f = new HasAttributeFilter("class", "internal");
+		Parser parser = new Parser();
+		parser.setResource(URL);
+		NodeList nl = parser.parse(f);
+		String str = null;
+		String link = null;
+
+		for (int i = 0; i < nl.size(); i++) {
+
+			Node node = nl.elementAt(i);
+			str = node.getFirstChild().getText().trim();
+
+			if (!str.contains("Fakul") && !str.contains("&auml;")
+					&& !str.contains("&uuml;") && !str.contains("&ouml;")
+					&& !str.contains("&Auml") && !str.contains("Weiter")
+					&& !str.contains("Double") && !str.contains("Joint")) {
+
+				if (node instanceof Tag) {
+					link = ((Tag) node).getAttribute("href");
+					studiesMap.put(str, URLPrefix + link);
+				}
+
+			}
+		}
+		return studiesMap;
+	}
+}
