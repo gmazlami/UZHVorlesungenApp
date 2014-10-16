e622d6de30d985216628a551f357ad4c6ca37733
Genc Mazlami <genc.mazlami@uzh.ch>
2014-10-16 23:15:06 +0200

    Fixed Medical Multipage parsing bug

--- src/com/example/uzhvorlesungen/parsers/VVZMedLecturesParser.java
+++ src/com/example/uzhvorlesungen/parsers/VVZMedLecturesParser.java
@@ -1,11 +1,14 @@
 package com.example.uzhvorlesungen.parsers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.htmlparser.Node;
 import org.htmlparser.Parser;
 import org.htmlparser.Tag;
+import org.htmlparser.filters.HasAttributeFilter;
 import org.htmlparser.filters.TagNameFilter;
 import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
@@ -22,23 +25,51 @@ public class VVZMedLecturesParser {
 
 	
 	public Map<String, String> parseSites() throws ParserException{
-		HashMap<String, String> map = new HashMap<String, String>();
-		Parser parser = new Parser();
-		parser.setResource(resourceURL);
-		
-		TagNameFilter filter = new TagNameFilter("tr");
-		NodeList nl = parser.parse(filter);
-		Node firstTrNode = nl.elementAt(1);
-		Node current = firstTrNode;
-		
-		while(current.getNextSibling() != null){
-			Node studyNameNode = current.getFirstChild().getNextSibling().getFirstChild();
-			map.put(studyNameNode.toPlainTextString(), getLinkForNode(studyNameNode));
-			current = current.getNextSibling();
+		HashMap<String, String> map = new HashMap<String,String>();
+		List<String> pages = parsePages();
+		for(String page : pages){
+			Parser parser = new Parser();
+			parser.setResource(page);
+			
+			TagNameFilter filter = new TagNameFilter("tr");
+			NodeList nl = parser.parse(filter);
+			Node firstTrNode = nl.elementAt(1);
+			Node current = firstTrNode;
+			
+			while(current != null){
+				if(current.getNextSibling() != null){
+					Node studyNameNode = current.getFirstChild().getNextSibling().getFirstChild();
+					map.put(Utils.fixUmlauts(studyNameNode.toPlainTextString()), getLinkForNode(studyNameNode));
+				}
+				current = current.getNextSibling();
+			}
 		}
-
+		
 
 		return map;
+	}
+	
+	private ArrayList<String> parsePages() throws ParserException{
+		ArrayList<String> pageList = new ArrayList<String>();
+		Parser parser = new Parser();
+		pageList.add(resourceURL);
+		parser.setResource(resourceURL);
+		HasAttributeFilter filter = new HasAttributeFilter("class", "pagination-current");
+		NodeList nl = parser.parse(filter);
+		Node currentPageNode = nl.elementAt(0).getNextSibling();
+		int page = 2;
+		while(currentPageNode != null){
+			pageList.add(createPageLink(page));
+			page++;
+			currentPageNode = currentPageNode.getNextSibling();
+		}
+		
+		System.out.println(pageList);
+		return pageList;
+	}
+	
+	private String createPageLink(int page){
+		return resourceURL + "?sortBy=&sortOrder=ascending&page=" + page;
 	}
 	
 	private String getLinkForNode(Node studyNode){
